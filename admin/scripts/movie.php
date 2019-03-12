628fe0e57381467b619cb3685e8441dec9edaf93
jaclynflomen <j_floman@fanshaweonline.ca>
2019-03-12 17:41:28 -0400

    added new movies

--- admin/scripts/movie.php
+++ admin/scripts/movie.php
@@ -20,9 +20,49 @@ function addMovie($cover, $title, $year, $run, $story, $trailer, $release, $genr
             throw new Exception('Failed to move uploaded file, check permission!');
         }
 
-        //4. Adding entried to the db (both tbl_movies and tbl_mov_genre)
+        $th_copy = '../images/TH_' . $cover['name'];
+        if(!copy($target_path, $th_copy)){
+            throw new Exception('Failed to move copy file, check permission!');
+        }
 
+        //4. Adding entried to the db (both tbl_movies and tbl_mov_genre)
+        $add_movies_query = 'INSERT INTO tbl_movies(movies_cover, movies_title, movies_year, movies_runtime, movies_storyline, movies_trailer, movies_release)';
+		$add_movies_query .= ' VALUES(:movies_cover,:movies_title,:movies_year,:movies_runtime, :movies_storyline, :movies_trailer, :movies_release)';
+
+		$add_movies = $pdo->prepare($add_movies_query);
+		$add_movies->execute(
+			array(
+				':movies_cover'=>$cover['name'],
+				':movies_title'=>$title,
+				':movies_year'=>$year,
+                ':movies_runtime'=>$run,
+                ':movies_storyline'=>$story,
+                ':movies_trailer'=>$trailer,
+                ':movies_release'=>$release
+                )
+		);
+
+        if(!$add_movies){
+            throw new Exception('Failed to insert the new movie!');
+        }
+        $last_id = $pdo->lastInsertId();
+        
+        $insert_genre_query = 'INSERT INTO tbl_mov_genre(movies_id, genre_id) VALUES(:movies_id, :genre_id)';
+        $insert_genre = $pdo->prepare($insert_genre_query);
+        $insert_genre->execute(
+            array(
+                ':movies_id'=>$last_id,
+                ':genre_id'=>$genre
+            )
+            );
+
+
+		if(!$insert_genre->rowCount()){
+			throw new Exception('Failed to set genre!');
+        }
+        
         //5. if all above works fine, redirect user to index.php
+        redirect_to('index.php');
 
     }catch(Exception $e){
         $error = $e->getMessage();
